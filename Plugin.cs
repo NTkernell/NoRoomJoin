8efb9de09e4556c58e4ff30944c5d47411bd15ef
NTkernel <ntkernel1@gmail.com>
2024-08-26 11:06:50 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using Photon.Pun;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace TestMod
+{
+   
+
+    
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        void Start()
+        {
+            
+            
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            
+           
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+           
+           
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            
+
+            PhotonNetwork.LeaveRoom();
+
+        }
+
+        void Update()
+        {
+            
+
+            PhotonNetwork.LeaveRoom();
+
+        }
+
+       
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+           
+
+            PhotonNetwork.LeaveRoom();
+
+            inRoom = true;
+        }
+
+        
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+           
+
+            PhotonNetwork.LeaveRoom();
+
+            inRoom = false;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMod
+{
+    
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ntkernel.gorillatag.NoRoomJoin";
+        public const string Name = "NoRoomJoin";
+        public const string Version = "1.0.1";
+    }
+}
